e6ef1330b6cee29948fb5ba43c38d1b0b2ffbf72
Osotov-s <82163862+Osotov-s@users.noreply.github.com>
2024-12-13 23:19:17 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+     if second != 0:
+         rez = first / second
+         return rez
+     else:
+         rez = 'Ошибка'
+         return rez
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        rez = first / second
+        return rez
+    else:
+        return inf
